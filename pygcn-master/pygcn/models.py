237ee03bd1adf34f094d03ca9980ecff7bd34c7e
bcsrn <39689482+bcsrn@users.noreply.github.com>
2019-04-22 11:07:07 -0500

    Added another GCN layer

--- pygcn-master/pygcn/models.py
+++ pygcn-master/pygcn/models.py
@@ -8,11 +8,14 @@ class GCN(nn.Module):
         super(GCN, self).__init__()
 
         self.gc1 = GraphConvolution(nfeat, nhid)
-        self.gc2 = GraphConvolution(nhid, nclass)
+        self.gc2 = GraphConvolution(nhid, 16)
+        self.gc3 = GraphConvolution(16, nclass)        
         self.dropout = dropout
 
     def forward(self, x, adj):
         x = F.relu(self.gc1(x, adj))
         x = F.dropout(x, self.dropout, training=self.training)
-        x = self.gc2(x, adj)
+        x = F.relu(self.gc2(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc3(x, adj)        
         return F.log_softmax(x, dim=1)
